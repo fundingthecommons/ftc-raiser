5f791aa7cd9345cf82ba82ad76f9e4a41e9f17e7
James Farrell <james@toucan.earth>
2024-10-17 14:56:13 +0700

    feat: add connect button to header

--- components/nav-bar.tsx
+++ components/nav-bar.tsx
@@ -2,17 +2,23 @@
 import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
+import ConnectButton from './connect-button';
 
 const Navbar = () => {
     return (
         <nav className="flex justify-between items-center bg-gradient-to-r from-blue-400 to-green-400 p-4 shadow-md">
             <div className="flex items-center space-x-4">
-                <Image src="/images/ftc-logo.png" alt="Funding the Commons Logo" width={100} height={29.5} />
-            
+                <Image src="/images/ftc-logo.png" alt="Funding the Commons Logo" width={200} height={58} />
             </div>
-            <div className="text-white text-sm font-medium">
-                Transforming funding models & mechanisms for public goods funding
+            <div className="flex items-center space-x-6">
+                <Link href="/organizations" className="text-white hover:text-gray-200 transition-colors">
+                    Organizations
+                </Link>
+                <Link href="/leaderboard" className="text-white hover:text-gray-200 transition-colors">
+                    Leaderboard
+                </Link>
             </div>
+            <div> <ConnectButton/></div>
         </nav>
     );
 };
