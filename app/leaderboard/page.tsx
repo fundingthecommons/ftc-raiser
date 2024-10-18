af791f742caeaff6df1df747beec68c55b01c5e9
pheuberger <philipp@pheuberger.com>
2024-10-18 22:51:16 +0700

    feat: make orgs page pretty
    Better layout for the cards themselves and also fixing the content to
    the top of the site.

--- app/leaderboard/page.tsx
+++ app/leaderboard/page.tsx
@@ -2,11 +2,11 @@
 import Leaderboard from "@/components/leaderboard";
 
 export default function Home() {
-	return (
-		<div className="flex flex-col min-h-screen">
-			<main className="flex flex-col items-center justify-center flex-grow p-8 gap-8">
-				<Leaderboard />
-			</main>
-		</div>
-	);
+  return (
+    <div className="flex flex-col min-h-screen">
+      <main className="flex flex-col items-center justify-center flex-grow p-8 gap-8">
+        <Leaderboard />
+      </main>
+    </div>
+  );
 }
