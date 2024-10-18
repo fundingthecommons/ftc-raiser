5e7166ca8117d586d127e1e9187b192a55cdb44b
bitbeckers <code@bitbeckers.com>
2024-10-19 00:08:25 +0700

    feat: token balances and dynamic symbol display

--- components/token-selector.tsx
+++ components/token-selector.tsx
@@ -1,43 +1,46 @@
 "use client";
 
-import { TokenBalance, TokenMetadataResponse } from "alchemy-sdk";
+import {TokenBalance, TokenMetadataResponse} from "alchemy-sdk";
 import {
-	Select,
-	SelectContent,
-	SelectItem,
-	SelectTrigger,
-	SelectValue,
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
 } from "@/components/ui/select";
+import {formatUnits} from "viem";
 
 export type ExtendedTokenBalance = TokenBalance & TokenMetadataResponse;
 
 export type TokenSelectorProps = {
-	tokens: ExtendedTokenBalance[];
-	selectedToken: string;
-	onSelectChange: (value: string) => void;
+    tokens: ExtendedTokenBalance[];
+    selectedToken: string;
+    onSelectChange: (value: string) => void;
 };
 
 export default function TokenSelector({
-	tokens,
-	selectedToken,
-	onSelectChange,
-}: TokenSelectorProps) {
-	return (
-		<div className="p-4">
-			<h2 className="text-xl mb-4">Token</h2>
-			<Select onValueChange={onSelectChange} value={selectedToken}>
-				<SelectTrigger className="w-[300px]">
-					<SelectValue placeholder="Select a token" />
-				</SelectTrigger>
-				<SelectContent>
-					{tokens.map((token, index) => (
-						<SelectItem key={index} value={token.contractAddress}>
-							{token.symbol || token.contractAddress}
-						</SelectItem>
-					))}
-				</SelectContent>
-			</Select>
-			{selectedToken && <p className="mt-4">Selected token: {selectedToken}</p>}
-		</div>
-	);
+                                          tokens,
+                                          selectedToken,
+                                          onSelectChange,
+                                      }: TokenSelectorProps) {
+    return (
+        <div className="p-4">
+            <h2 className="text-xl mb-4">Token</h2>
+            <Select onValueChange={onSelectChange} value={selectedToken}>
+                <SelectTrigger className="w-[300px]">
+                    <SelectValue placeholder="Select a token"/>
+                </SelectTrigger>
+                <SelectContent>
+                    {tokens.map((token, index) => (
+                        <SelectItem key={index} value={token.contractAddress}>
+                            {token.symbol || token.contractAddress}{"  "}
+                            {token.tokenBalance && <span
+                                className="text-xs text-gray-500">{Number(formatUnits(BigInt(token.tokenBalance), token.decimals)).toFixed(4)}</span>}
+                        </SelectItem>
+                    ))}
+                </SelectContent>
+            </Select>
+            {selectedToken && <p className="mt-4">Selected token: {selectedToken}</p>}
+        </div>
+    );
 }
