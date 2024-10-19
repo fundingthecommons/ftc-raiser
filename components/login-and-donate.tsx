1f5ce71db5b2e9694cfc236a8ca7167e4a71e12b
pheuberger <philipp@pheuberger.com>
2024-10-19 10:56:11 +0700

    feat: make donate card hover over over banner
    This moves the card up and creates a nice visual effect.

--- components/login-and-donate.tsx
+++ components/login-and-donate.tsx
@@ -3,9 +3,11 @@
 import SwapForm from "@/components/swap-form";
 
 export default function LoginAndDonate() {
-	return (
-		<div className="container mx-auto p-4">
-			<SwapForm />
-		</div>
-	);
+  return (
+    <div className="container mx-auto p-4 relative">
+      <div className="absolute top-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full max-w-md">
+        <SwapForm />
+      </div>
+    </div>
+  );
 }
